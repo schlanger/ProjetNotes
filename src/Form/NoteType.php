a6ceba652c7de5bc8f6212b72b6bd48c6ffc8508
schlanger <yehoudaschlanger@gmail.com>
2024-05-02 16:41:35 +0200

    CRUD de l'entité Note

---
+++ src/Form/NoteType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Note;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('note',
+                NumberType::class,
+                [
+                    'label' => 'Note',
+                    'attr' => [
+                        'placeholder' => 'Note',
+                        'class' => 'form-control',
+                    ],
+                ])
+            ->add('description',
+                TextType::class,
+                [
+                    'label' => 'Description',
+                    'attr' => [
+                        'placeholder' => 'Description',
+                        'class' => 'form-control',
+                    ],
+                ])
+            ->add('coeff',
+                NumberType::class,
+                [
+                    'label' => 'Coefficient',
+                    'attr' => [
+                        'placeholder' => 'Coefficient',
+                        'class' => 'form-control',
+                    ],
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Note::class,
+        ]);
+    }
+}
